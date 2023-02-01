e858c120235293d4c35dc9a642aa20baff9f713a
karolinyoliveira <karolinyoliveira@usp.br>
2023-01-31 23:31:22 -0300

    update joguinhos section

--- happy-birthday-nunu/src/components/ProjectCard.js
+++ happy-birthday-nunu/src/components/ProjectCard.js
@@ -1,4 +1,5 @@
 import { Col } from "react-bootstrap";
+import museu from "../assets/img/museu.jpeg";
 
 export const ProjectCard = ({ title, description, imgUrl }) => {
   return (
--- happy-birthday-nunu/src/components/Projects.js
+++ happy-birthday-nunu/src/components/Projects.js
@@ -1,43 +1,66 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/primeira-foto.jpg";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
-//import colorSharp2 from "../assets/img/color-sharp2.png";
+import primeira from "../assets/img/primeira-foto.jpg";
+import florzinha from "../assets/img/florzinha.jpg";
+import caco from "../assets/img/caco.jpg";
+import caous from "../assets/img/caous.jpg";
+import banguela from "../assets/img/banguela.jpg";
+import mae from "../assets/img/mae.jpg";
+import pai from "../assets/img/pai.jpg";
+import trem from "../assets/img/trem.jpg";
+import bath from "../assets/img/bath.jpeg";
+
 import "animate.css";
 import TrackVisibility from "react-on-screen";
 
 export const Projects = () => {
   const joguinho = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
+      title: "#1",
+      description: "Onde eu tirei a segunda foto que já tirei sua?",
+      imgUrl: primeira,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
+      title: "#2",
+      description: "Quando nosso filhote faz aniversário?",
+      imgUrl: caous,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
+      title: "#3",
+      description: "Um sonho em comum que nós temos pra daqui uns 10 anos",
+      imgUrl: caco,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
+      title: "#4",
+      description:
+        "Qual era o nome da cor que o dono pediu pra pintar seu quase futuro apê?",
+      imgUrl: mae,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
+      title: "#5",
+      description: "Do que era o museu que a gente foi em Piracicaba?",
+      imgUrl: pai,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
+      title: "#6",
+      description: "O que você não pode fazer comigo?",
+      imgUrl: trem,
+    },
+    {
+      title: "#7",
+      description:
+        "Qual é o lugar em que mais tiramos fotos nos últimos meses?",
+      imgUrl: bath,
+    },
+    {
+      title: "#8",
+      description: "Qual criatura mística nós temos em comum?",
+      imgUrl: banguela,
+    },
+    {
+      title: "#9",
+      description: "Nada não, só te achei linda nessa e decidi deixar",
+      imgUrl: florzinha,
     },
   ];
 
@@ -65,14 +88,16 @@ export const Projects = () => {
                   <h2>Joguinho</h2>
 
                   <p>
+                    <p></p>
                     Mô, seguinte!<br></br>
-                    Pensei em alguns momentos especiais nossos e pra deixar isso
-                    mais divertido você precisa acertar
+                    Pensei em alguns momentos especiais e pra deixar isso mais
+                    divertido você precisa acertar umas perguntinhas para
+                    desbloquear essas lembranças
                   </p>
 
                   <Row>
-                    {joguinho.map((foto, index) => {
-                      return <ProjectCard key={index} {...foto} />;
+                    {joguinho.map((photo, index) => {
+                      return <ProjectCard key={index} {...photo} />;
                     })}
                   </Row>
                 </div>
